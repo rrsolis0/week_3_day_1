da33c5662133c93006445334c71c815e76e5aab4
Solis-Chavez <rsolis12@cps.k12.il.us>
2024-09-10 15:03:54 -0500

    added more content

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -72,6 +72,16 @@ print( name + " liked to hum the song " + song + " while coding. It was so annoy
 # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
 print("Still, " + name + " was the best coder on the team and could write " + str(number) + " lines of code everyday. Maybe the " + song + " was " + name + "'s secret power?") 
 
+number1 = 100
+number2 = 200  
+number3 = 300
+number4 = 400
+number5 = 500
+# put all of these into one sentence
+# print(str(number1) + ", " + str(number2) + ", " +str(number3)+ ", " + str(number4) + ", " + str(number5) + ".")
+# f strings
+print(f"{number1}, {number2}, {number3}, {number4}, {number5}.")
+
 ##########################################################################################
 
 
