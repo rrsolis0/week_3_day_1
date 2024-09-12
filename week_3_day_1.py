438809bf916cd936a477d38d1c5df1e5a8ba71f0
Solis-Chavez <rsolis12@cps.k12.il.us>
2024-09-12 14:38:42 -0500

    added more content

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -183,20 +183,36 @@ print(f"{number1}, {number2}, {number3}, {number4}, {number5}.")
 
 ##########################################################################################
 # #addition
+print(2+1)
 # #multiplication
+print(2*2)
 # #division
+print(3/2)
 # #modulo
+print(7%4) #remainder of two numbers divided
 # #powers
+print(2**3) 
 # #get the max and min of a number
+print(max(1,2)) #max number get the largest number
+print(min(1,2)) # min number get the smallest number
 # #round a number
+print(round(3.4))
 # # absolute value
+print(abs(-3 )) #absoulte value of a number 
+# absolute value is always the distance from 0 to the number 
+#its always positive
+
 # # order of operations
+print(2+10*10+3)
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *    
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print(floor(3.7)) #floor method rounds down
 # #ceil method
+print(ceil(3.7)) #ceil method rounds up
 # #sqrt method
+print(sqrt(36))
 
 
 
@@ -216,14 +232,23 @@ print(f"{number1}, {number2}, {number3}, {number4}, {number5}.")
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
+name = input("what is your name?")
+print("hello" + name)
 # # basic math calculator
 # #ask the user for 2 numbers
+num1 = int(input("eneter a number"))
+num2= int(input("enter another number"))
 # # print out a statement where you:
 # # add them together
+print(num1 + num2)
 # #multiply
+print(num1*num2)
 # # find the max number
+print(max(num1,num2))
 # # find the remainder of the numbers
+print(num1%num2)
 # #round one number
+print(ceil(num1))
 
 
 
